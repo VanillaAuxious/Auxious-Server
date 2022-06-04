178192224a662a1ccf1e2fe090078bb7b80e0a95
hkh9601 <hkh9601@gmail.com>
2022-06-05 01:00:29 +0900

    autologinandgetpagedata

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const loaders = require('./loader/index');
+const run = require('./services/crawlingService');
 const app = express();
 
 (async function () {
   await loaders({ expressApp: app });
+  run();
 })();
 
 module.exports = app;
