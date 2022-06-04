5e388f941825b312ccf6d7a00b171f119cc967c0
hkh9601 <hkh9601@gmail.com>
2022-06-05 02:52:53 +0900

    test

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const loaders = require('./loader/index');
 const app = express();
-const run = require('./services/crawlingService');
 
 (async function () {
-  run();
   await loaders({ expressApp: app });
 })();
 
