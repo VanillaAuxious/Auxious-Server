843ebdd7f548ac20ba4a82b8a77579d403ef199e
yb9255 <yoobin9255@gmail.com>
2022-06-12 05:23:36 +0900

    서버가 정상적으로 json을 리턴하는지에 대한 체크

--- loader/router.js
+++ loader/router.js
@@ -3,7 +3,7 @@ const usersRouter = require('../routes/users');
 const buildingsRouter = require('../routes/buildings');
 
 function routerLoader({ app }) {
-  app.use('/api', indexRouter);
+  app.use('/', indexRouter);
   app.use('/api/users', usersRouter);
   app.use('/api/buildings', buildingsRouter);
 }
