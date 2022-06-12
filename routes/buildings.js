9059fcc6aeaaace1c5098e0000dea994eae02efa
hkh9601 <hkh9601@gmail.com>
2022-06-13 05:11:31 +0900

    그래프데이터엔드포인트추가

--- routes/buildings.js
+++ routes/buildings.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   getBuildingInfo,
   getBuildingsOnMap,
+  getAuctions
 } = require('../controllers/building.controller');
 const { isLoggedIn } = require('../middlewares/auth');
 
 router.get('/', isLoggedIn, getBuildingsOnMap);
+router.get('/auctions', isLoggedIn, getAuctions);
 router.get('/:buildingId', isLoggedIn, getBuildingInfo);
 
 module.exports = router;
