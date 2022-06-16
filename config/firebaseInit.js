cb24742e1a48570feeab86b55674e0218a93aede
yb9255 <yoobin9255@gmail.com>
2022-06-16 21:04:40 +0900

    escape 코드 삭제

--- config/firebaseInit.js
+++ config/firebaseInit.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   type: process.env.SERVICE_ACCOUNT_TYPE,
   project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
   private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
   client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
   client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
   auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
