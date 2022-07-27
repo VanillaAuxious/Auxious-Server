4bbb8e29d32a8f119992b6872e47736bf2d38268
임유빈 <yoobin@slvitui-iMac.local>
2022-07-28 02:05:01 +0900

    홈페이지 프린트 메세지 변경

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json({
-    ok: true,
-    status: 200,
-    message: 'this is a server',
-    cliEnvIsWorking: !!process.env.ENV,
-  });
+  res.send('hi');
 });
 
 module.exports = router;
